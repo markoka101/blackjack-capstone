4982619837c5a47460fab33ef057c4f8307ad5f3
markoka101 <markoka101@gmail.com>
2023-07-17 20:23:46 -0400

    Created new class to store game information during hand

---
+++ blackjack/src/main/java/com/example/demo/pojo/GameInfo.java
@@ -0,0 +1,14 @@
+package com.example.demo.pojo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Embeddable
+public class GameInfo {
+}
